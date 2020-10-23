86035ed4cd71aa0697d653ab5361b7fed41ad139
Jesus Juarez <jjayala1@gmail.com>
2020-10-23 12:25:30 -0500

    se ocultan inputs al mostrar las palabras

--- lista.js
+++ lista.js
@@ -6,21 +6,25 @@ const abreLista = (listWords) =>{
 
     const data = listWords.map( (x, index) => {
 
+        //DIV de la tarjeta completa
         let newDiv = document.createElement("div");
         newDiv.className =   "fl db h-100 w-90 w-30-ns tc bg-light-blue br3 pa3 ma2 pointer grow shadow-5";
         newDiv.style.height = "20em"
         newDiv.id = 'D'+index;
 
+        //DIV para la palabra
         let newDivWord = document.createElement("div");
         newDivWord.className = "dn tc ma2 ttu tracked mt0 dark-blue"
         let newWord = document.createTextNode(x.word);
         newDivWord.appendChild(newWord);
 
+        //DIV para la definicion
         let newDivDefinition = document.createElement("div");
         let newDefinition = document.createTextNode(x.definition);
         newDefinition.className = ""
         newDivDefinition.appendChild(newDefinition);
 
+        //DIV para el ejemplo
         let newDivExample = document.createElement("div");
         newDivExample.className = "dn ma2"
         let newExample = document.createTextNode(x.example);
@@ -28,6 +32,7 @@ const abreLista = (listWords) =>{
         newDivExample.style.color = "green"
         newDivExample.appendChild(newExample);
 
+        //DIV para la imagen
         let newDivImg = document.createElement("div");
         newDivImg.className = "h4 ma2"
         let newImage = document.createElement("img");
@@ -35,7 +40,9 @@ const abreLista = (listWords) =>{
         newImage.className = "h4";
         newDivImg.appendChild(newImage);
 
+        //DIV para el input
         let newDivInput = document.createElement("div");
+        newDivInput.className = "db1"
         let newInput = document.createElement("input");
         newInput.setAttribute('type', 'text');
         newInput.setAttribute('correct', x.word);
@@ -43,6 +50,7 @@ const abreLista = (listWords) =>{
         newInput.addEventListener("keydown", logKey);
         newDivInput.appendChild(newInput);
 
+        //Append todos los divs
         newDiv.appendChild(newDivImg);
         newDiv.appendChild(newDivWord);
         newDiv.appendChild(newDivDefinition);
@@ -54,11 +62,13 @@ const abreLista = (listWords) =>{
     });
 
     let divs = document.querySelectorAll(".dn");
+    let inputs = document.querySelectorAll(".db1");
 
     let verRespuestas = document.getElementById("respuestas");
     respuestas.addEventListener('click', () =>{
         console.log('click');
         divs.forEach( el => el.classList.toggle("dn"))
+        inputs.forEach( el => el.classList.toggle("dn"))
         respuestas.classList.toggle("gradient-blue");
     });
 
