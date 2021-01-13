f1f597441d3292bbaf5eb329338c50e7cbe4ebb7
Jesus Juarez <jjayala1@gmail.com>
2021-01-13 14:54:14 -0600

    add list 13 corrections

--- lists/lista13.js
+++ lists/lista13.js
@@ -1,4 +1,4 @@
-listWords6 = [
+listWords13 = [
     {
         "word": "option",
         "definition": "One thing that can be chosen from a set of possibilities.",
@@ -7,116 +7,116 @@ listWords6 = [
     },
     {
         "word": "position",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W02.png"
     },
     {
         "word": "action",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W03.png"
     },
     {
         "word": "section",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W04.png"
     },
     {
         "word": "mention",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W05.png"
     },
     {
         "word": "question",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W06.png"
     },
     {
         "word": "direction",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W07.png"
     },
     {
         "word": "suggestion",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W08.png"
     },
     {
         "word": "infection",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W09.png"
     },
     {
         "word": "rejection",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W10.png"
     },
     {
         "word": "condition",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W11.png"
     },
     {
         "word": "function",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W12.png"
     },
     {
         "word": "competition",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W13.png"
     },
     {
         "word": "contribution",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W14.png"
     },
     {
         "word": "introduction",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W15.png"
     },
     {
         "word": "opposition",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W16.png"
     },
     {
         "word": "production",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W17.png"
     },
     {
         "word": "connection",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W18.png"
     },
     {
         "word": "experimentation",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W19.png"
     },
     {
         "word": "hospitalization",
-        "definition": "",
-        "example": "",
+        "definition": "1",
+        "example": "1",
         "image": "L13W20.png"
     }
 ]
