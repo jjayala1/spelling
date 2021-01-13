ee564d6a900042f06075065ec6aed56a884516c7
Jesus Juarez <jjayala1@gmail.com>
2021-01-13 14:31:49 -0600

    add list 13

---
+++ lists/lista13.js
@@ -0,0 +1,122 @@
+listWords6 = [
+    {
+        "word": "option",
+        "definition": "",
+        "example": "",
+        "image": "L13W01.png"
+    },
+    {
+        "word": "position",
+        "definition": "",
+        "example": "",
+        "image": "L13W02.png"
+    },
+    {
+        "word": "action",
+        "definition": "",
+        "example": "",
+        "image": "L13W03.png"
+    },
+    {
+        "word": "section",
+        "definition": "",
+        "example": "",
+        "image": "L13W04.png"
+    },
+    {
+        "word": "mention",
+        "definition": "",
+        "example": "",
+        "image": "L13W05.png"
+    },
+    {
+        "word": "question",
+        "definition": "",
+        "example": "",
+        "image": "L13W06.png"
+    },
+    {
+        "word": "direction",
+        "definition": "",
+        "example": "",
+        "image": "L13W07.png"
+    },
+    {
+        "word": "suggestion",
+        "definition": "",
+        "example": "",
+        "image": "L13W08.png"
+    },
+    {
+        "word": "infection",
+        "definition": "",
+        "example": "",
+        "image": "L13W09.png"
+    },
+    {
+        "word": "rejection",
+        "definition": "",
+        "example": "",
+        "image": "L13W10.png"
+    },
+    {
+        "word": "condition",
+        "definition": "",
+        "example": "",
+        "image": "L13W11.png"
+    },
+    {
+        "word": "function",
+        "definition": "",
+        "example": "",
+        "image": "L13W12.png"
+    },
+    {
+        "word": "competition",
+        "definition": "",
+        "example": "",
+        "image": "L13W13.png"
+    },
+    {
+        "word": "contribution",
+        "definition": "",
+        "example": "",
+        "image": "L13W14.png"
+    },
+    {
+        "word": "introduction",
+        "definition": "",
+        "example": "",
+        "image": "L13W15.png"
+    },
+    {
+        "word": "opposition",
+        "definition": "",
+        "example": "",
+        "image": "L13W16.png"
+    },
+    {
+        "word": "production",
+        "definition": "",
+        "example": "",
+        "image": "L13W17.png"
+    },
+    {
+        "word": "connection",
+        "definition": "",
+        "example": "",
+        "image": "L13W18.png"
+    },
+    {
+        "word": "experimentation",
+        "definition": "",
+        "example": "",
+        "image": "L13W19.png"
+    },
+    {
+        "word": "hospitalization",
+        "definition": "",
+        "example": "",
+        "image": "L13W20.png"
+    }
+]
